badc7c7ee8f270c540ea45a786c551d2f6a272b8
Wes Rice <wrice@maxmind.com>
2020-11-03 17:19:37 -0500

    fix: remove passing of commit message to commitizen

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,7 +2,6 @@ module.exports = {
   hooks: {
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
     'pre-commit': 'lint-staged',
-    'prepare-commit-msg':
-      "grep -qE '^[^#]' .git/COMMIT_EDITMSG || (exec < /dev/tty && git cz --hook || true)",
+    'prepare-commit-msg': 'exec < /dev/tty && git cz --hook || true',
   },
 };
