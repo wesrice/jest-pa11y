82c96a3ec84b8d07001fc822aa979b99c9577a42
Wes Rice <wrice@maxmind.com>
2020-11-03 11:59:08 -0500

    refactor: remove dependency of third-part Semantic Release config

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,4 @@
 module.exports = {
-  extends: '@jedmao/semantic-release-npm-github-config',
   branches: [
     '+([0-9])?(.{+([0-9]),x}).x',
     'main',
@@ -7,5 +6,35 @@ module.exports = {
     'next-major',
     { name: 'beta', prerelease: true },
     { name: 'alpha', prerelease: true },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogTitle:
+          '# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines.',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        tarballDir: 'pack',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: 'pack/*.tgz',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
 };
