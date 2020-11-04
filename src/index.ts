1d59149ee775715f4a5b1ba978063512fba081ec
Wes Rice <wrice@maxmind.com>
2020-11-04 09:32:08 -0500

    fix: ensure `toHaveNoPa11yViolations` is exported from module

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,13 @@ import environment from './environment';
 import globalSetup from './globalSetup';
 import globalTeardown from './globalTeardown';
 import runPa11y from './runPa11y';
+import toHaveNoPa11yViolations from './toHaveNoPa11yViolations';
 
-export { configurePa11y, environment, globalSetup, globalTeardown, runPa11y };
+export {
+  configurePa11y,
+  environment,
+  globalSetup,
+  globalTeardown,
+  runPa11y,
+  toHaveNoPa11yViolations,
+};
